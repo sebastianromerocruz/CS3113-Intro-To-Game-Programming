5d4b6d853ac1b96d42d85b460d4040084e7ae9e0
Sebastián <src402@nyu.edu>
2026-02-06 15:45:35 -0500

    Addo pensum II

---
+++ exercises/02-textures/README.md
@@ -0,0 +1,87 @@
+<h2 align=center>Week 03</h2>
+
+<h1 align=center>Textures: <em>Feel Good Inc.</em></h1>
+
+<h4 align=center><a href="assets/02-textures.zip">Download The Project's Zip File Here</a><h4>
+
+---
+
+## Sections
+1. [**The Problem**](#1)
+2. [**The Requirements**](#2)
+3. [**How and What to Submit**](#3)
+
+---
+
+<a id="1"></a>
+
+## The Problem
+
+If you run [**`main.cpp`**](main.cpp), you will see the following screen:
+
+<a id="fg-1"></a>
+
+<p align=center>
+    <img src="assets/start.gif">
+    </img>
+</p>
+
+<p align=center>
+    <sub>
+        <strong>Figure I</strong>: Your starting condition. All rights go to the iconic <a href="https://en.wikipedia.org/wiki/Gorillaz"><strong>Gorillaz™</strong></a>.
+    </sub>
+</p>
+
+Your goal is to [**manipulate the texture's UV-coordinates**](https://github.com/sebastianromerocruz/CS-3113-Intro-To-Game-Programming/tree/main/lectures/03-textures#2-2-1) in order for the following to happen:
+
+<a id="fg-2"></a>
+
+<p align=center>
+    <img src="assets/end.gif">
+    </img>
+</p>
+
+<p align=center>
+    <sub>
+        <strong>Figure II</strong>: Your goal.
+    </sub>
+</p>
+
+In other words:
+
+1. Smooth out the scaling to be frame-independent/time-dependent.
+2. Isolate one of the four members every _100 frames_.
+3. Make that member's quadrant the entire texture applied to the game object.
+
+Note the enum defined at the top of the file with each member's names, starting from the top left, going clockwise:
+
+```cpp
+enum Member { MURDOC, TWO_D, RUSSEL, NOODLE };
+```
+
+Note also that this project uses `cs3113.cpp` and `cs3113.h`. In your `makefile`, but sure to change the source files to the following:
+
+```sh
+# Source Files
+SRCS = main.cpp CS3113/cs3113.cpp
+```
+
+<br>
+
+<a id="2"></a>
+
+## The Requirements
+
+- You _must_ manually calculate and use delta time
+- You _must_ adhere to only concepts learned in class so far.
+- You must try to minimise the amount of hard-coding (i.e. create as many constants as possible!)
+- If your finger even _hovers_ over the Copilot button on VSCode, I _swear to you_ that swift and merciless justice will fall upon your grade
+
+<br>
+
+<a id="3"></a>
+
+## How and What to Submit
+
+1. Show your working solution to the professor. All group members must have it working on their computers for the whole team to get checked out.
+2. You must submit in the relevant [**"discussion" board**](https://brightspace.nyu.edu/d2l/le/501465/discussions/List?dst=1) on Brightspace. **Only one person per team must upload the team's solution, but that person must include everybody's names**.
---
+++ exercises/02-textures/main.cpp
@@ -0,0 +1,136 @@
+#include "CS3113/cs3113.h"
+#include <math.h>
+
+enum Member { MURDOC, TWO_D, RUSSEL, NOODLE };
+
+// Global Constants
+constexpr int   SCREEN_WIDTH  = 1600 / 2,
+                SCREEN_HEIGHT = 900 / 2,
+                FPS           = 60,
+                SIZE          = 200,
+                FRAME_LIMIT   = 100;
+constexpr float MAX_AMP       = 10.0f;
+
+constexpr char    BG_COLOUR[] = "#000000";
+constexpr Vector2 ORIGIN      = { SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
+constexpr Vector2 BASE_SIZE   = { (float) SIZE, (float) SIZE };
+
+// Image owned by Gorillaz @see https://gorillaz.com/
+constexpr char ALBUM_COVER_FP[] = "assets/demon_days.png";
+
+// Global Variables
+AppStatus gAppStatus     = RUNNING;
+float     gScaleFactor   = SIZE,
+          gAngle         = 0.0f,
+          gPulseTime     = 0.0f;
+Vector2   gPosition      = ORIGIN;
+Vector2   gScale         = BASE_SIZE;
+
+Texture2D gTeardropTexture;
+
+// Function Declarations
+void initialise();
+void processInput();
+void update();
+void render();
+void shutdown();
+
+// Function Definitions
+void initialise()
+{
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Textures");
+
+    gTeardropTexture = LoadTexture(ALBUM_COVER_FP);
+
+    SetTargetFPS(FPS);
+}
+
+void processInput() 
+{
+    if (WindowShouldClose()) gAppStatus = TERMINATED;
+}
+
+void update() 
+{
+    /**
+     * @todo Calculate delta time
+     */
+
+
+    /**
+     * @todo Apply delta time to the time-dependent logic
+     */
+    gPulseTime += 1.0f;
+
+    gScale = {
+        BASE_SIZE.x + MAX_AMP * cos(gPulseTime),
+        BASE_SIZE.y + MAX_AMP * cos(gPulseTime)
+    };
+
+    /**
+     * @todo Switch member every 100 fames
+     */
+}
+
+void render()
+{
+    BeginDrawing();
+    ClearBackground(ColorFromHex(BG_COLOUR));
+
+    /**
+     * @todo Design your UV coordinates (i.e. textureArea) so that only one
+     * member is being rendered onto the screen.
+     */
+    Rectangle textureArea = {
+        // top-left corner
+        0.0f, 0.0f,
+
+        // bottom-right corner (of texture)
+        static_cast<float>(gTeardropTexture.width),
+        static_cast<float>(gTeardropTexture.height)
+    };
+
+    // Destination rectangle – centred on gPosition
+    Rectangle destinationArea = {
+        gPosition.x,
+        gPosition.y,
+        static_cast<float>(gScale.x),
+        static_cast<float>(gScale.y)
+    };
+
+    // Origin inside the source texture (centre of the texture)
+    Vector2 objectOrigin = {
+        static_cast<float>(gScale.x) / 2.0f,
+        static_cast<float>(gScale.y) / 2.0f
+    };
+
+    // Render the texture on screen
+    DrawTexturePro(
+        gTeardropTexture, 
+        textureArea, 
+        destinationArea, 
+        objectOrigin, 
+        gAngle, 
+        WHITE
+    );
+
+    EndDrawing();
+}
+
+void shutdown() { CloseWindow(); }
+
+int main(void)
+{
+    initialise();
+
+    while (gAppStatus == RUNNING)
+    {
+        processInput();
+        update();
+        render();
+    }
+
+    shutdown();
+
+    return 0;
+}
